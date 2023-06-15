9447722e32762bce1d35d0c480a091863f1fb43a
dave352 <dave@musma.net>
2023-06-15 20:33:11 +0900

    chore: AppLayout 폴더이동

--- src/layout/AppLayout/AppLayout.tsx
+++ src/layout/AppLayout/AppLayout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom'
 
-import { AppShell, Header, NavBar, NavBarLink } from '@musma/react-component'
+import { AppShell, NavBar, NavBarLink } from '@musma/react-component'
 import { FillDashboardIcon } from '@musma/react-icons'
 import { observer } from 'mobx-react-lite'
 
 import { RoutePaths } from 'src/routes'
 
+import { Header } from '../Header'
+
 export const AppLayout = observer(() => {
   return (
     <AppShell
-      header={<Header>Header</Header>}
+      header={<Header />}
       navBar={
         <NavBar>
           <NavBarLink icon={FillDashboardIcon} to={RoutePaths.대시보드} label="대시보드" />
